955e99f97a2ef8e632ac99c182ddfc7fd4c0978a
Christopher <chris.hunt.sa@gmail.com>
2023-04-03 13:37:34 +0200

    Create UI for Create and View Users

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp;
+using HttpClients.ClientInterfaces;
+using HttpClients.Implementations;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<IUserService, UserHttpClient>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
--- Domain/DTOs/UserCreationDto.cs
+++ Domain/DTOs/UserCreationDto.cs
@@ -2,5 +2,8 @@ namespace Domain.DTOs;
 
 public class UserCreationDto
 {
-    
+    public UserCreationDto(string username)
+    {
+        throw new NotImplementedException();
+    }
 }
