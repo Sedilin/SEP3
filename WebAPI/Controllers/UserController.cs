e7e866782c8b769b4c05ece65b04490e52efc15d
Sedilin <sramkowskilukasz@gmail.com>
2023-04-19 11:20:16 +0200

    Blazor and WEBApi without application layer

--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -38,18 +38,24 @@ public class UserController : ControllerBase
     }
     
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<User>>> GetAsync( string? username)
+    public async Task<IEnumerable<User>> GetAsync( string? username = null)
     {
-        try
+        string uri = "/user/";
+        if (!string.IsNullOrEmpty(username))
         {
-            SearchUserParametersDto parameters = new(username);
-            IEnumerable<User> users = await userLogic.GetAsync(parameters);
-            return Ok(users);
+            uri += $"?username={username}";
         }
-        catch (Exception e)
+        HttpResponseMessage response = await client.GetAsync(uri);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
         {
-            Console.WriteLine(e);
-            return StatusCode(500, e.Message);
+            throw new Exception(result);
         }
+
+        IEnumerable<User> users = JsonSerializer.Deserialize<IEnumerable<User>>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return users;
     }
 }
